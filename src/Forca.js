6c56fffc674c9a2476b8acb2acaf1c3633aa6331
JoaoVitorWinter <joaovwinter3108@gmail.com>
2023-09-28 00:12:19 -0300

    Forca continuada | 28/09 00:12

--- src/Forca.js
+++ src/Forca.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import { View, Text, Pressable, TextInput, StyleSheet } from "react-native";
 
 export default function Forca(props) {
+    let erros = 0;
     const [chute, setChute] = useState("");
+    const [letrasJogadas, setLetrasJogadas] = useState("");
     let array = props.palavra.split("");
     array = array.map((letra) => {
         if (letra != " ") {
@@ -11,39 +13,96 @@ export default function Forca(props) {
 
         return "  "
     });
-
     const [letras, setLetras] = useState(array);
 
+    useEffect(() => {
+        verificarVitoria();
+    }, [letras]);
 
     const fazerChute = (chute) => {
-        if (chute.length == 1) {
+        chute = chute.toLowerCase();
+        if (chute.length == 1)) {
+            setLetrasJogadas(letrasJogadas + ` ${chute} `)
+            var newLetras = [...letras];
+            var possuiLetra = false;
             for (let indice = 0; indice < props.palavra.length; indice++) {
-                console.log("Teste");
+                if (props.palavra.charAt(indice).toLowerCase() == chute) {
+                    newLetras[indice] = `${props.palavra.charAt(indice)}`;
+                    possuiLetra = true;
+                }
+            }
+
+            if (possuiLetra) {
+                setLetras(newLetras);
+            } else {
+                erros++;
+                console.log(`Quantidade de erros: ${erros}`);
+                if (erros == 6) {
+                    alert(`Você perdeu! A palavra secreta era ${props.palavra}`)
+                }
             }
         } else if (chute.length == props.palavra.length) {
-
+            if (chute == props.palavra.toLowerCase()) {
+                setLetras(props.palavra.split(""));
+            }
         }
     }
-    return (
-            <View>
-                <View>
 
-                </View>
-                <View>
-                    <Text>{letras}</Text>
-                </View>
-                <View>
-                    <TextInput style={styles.input} placeholder={"Seu chute"} onChangeText={setChute}></TextInput>
-                    <Pressable style={styles.botao} onPress={() => {
-                        fazerChute(chute);
-                    }}><Text style={styles.texto}>Chutar</Text></Pressable>
-                </View>
+    const verificarVitoria = () => {
+        for(let indice = 0; indice < props.palavra.length; indice++) {
+            if (letras[indice] == "_ ") {
+                return;
+            }
+        }
+
+        alert(`Você ganhou! A palavra secreta era ${props.palavra}`);
+        voltarHome();
+    }
+
+    const voltarHome = () => {
+        props.changeScreen("Home");
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.divForca}>
+
             </View>
-        );
-    
+            <View style={styles.divJogo}>
+                <Text>{letrasJogadas}</Text>
+                <Text>{letras}</Text>
+            </View>
+            <View style={styles.divChute}>
+                <TextInput style={styles.input} placeholder={"Seu chute"} onChangeText={setChute}></TextInput>
+                <Pressable style={styles.botao} onPress={() => {
+                    fazerChute(chute);
+                }}><Text style={styles.texto}>Chutar</Text></Pressable>
+                <Pressable style={styles.botao} onPress={voltarHome}><Text style={styles.texto}>Voltar</Text></Pressable>
+            </View>
+        </View>
+    );
+
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 20,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    divForca: {
+
+    },
+    divJogo: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    divChute: {
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 10,
+    },
     input: {
         width: 300,
         height: 30,
